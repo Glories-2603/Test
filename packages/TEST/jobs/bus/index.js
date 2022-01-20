2329b1f3e24598a772111051a347e83f2eb61a4c
Glories-2603 <53260516+Glories-2603@users.noreply.github.com>
2022-01-20 17:35:25 +0300

    UPLOAD111

--- packages/TEST/jobs/bus/index.js
+++ packages/TEST/jobs/bus/index.js
@@ -52,3 +52,23 @@ bus.load = function() {
 
 
 bus.load();
+
+
+
+mp.events.add('playerCommand', (player, command) => {
+    try {
+        if (command.toLowerCase() === "startjob") {
+            if (!player.seat == 0) return player.notify(`Необходимо быть за рулем!`);
+            if(!player.vehicle) return player.notify(`Необходимо быть в транспорте!`);
+            if(!player.vehicle.getVariable("job") == "bus") return player.notify(`Доступно только в автобусе!`);
+            player.call("client:busJob", [1]);
+            player.setVariable("job", "bus");
+       }
+       else {
+            player.notify(`Команды не существует на сервере!`);
+       }
+    }
+    catch (e) {
+        console.log(e);
+    }
+});
